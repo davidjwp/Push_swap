e4428114cada04dbcc5303ebf2809b42c2002e28
David JACOBS <djacobs@e1r4p16.clusters.42paris.fr>
2023-03-15 19:32:05 +0100

    update

--- sort_5.c
+++ sort_5.c
@@ -101,35 +101,8 @@
 // 	return (insts);
 // }
 
-
-int	sort_mid_5(t_list **lsta, t_list **lstb, t_range range, t_inst **insts)//21
-{
-	range.mid = (*lsta)->value;
-	*lsta = (*lsta)->next;
-	*lsta = (*lsta)->next;
-	if (range.mid < (*lsta)->value) 
-	{
-		*lsta = ft_lstfirst(lsta);
-		return (inst_sa(lsta, insts, 1), 0);
-	}
-	*lsta = (*lsta)->next;
-	if (range.mid < (*lsta)->value && (*lsta)->value != range.high)
-	{
-		*lsta = ft_lstfirst(lsta);
-		inst_sa(lsta, insts, 1);
-		inst_ra(lsta, insts, 1);
-		return(inst_sa(lsta, insts, 1), inst_rra(lsta, insts, 1), 0);
-	}
-	else 
-	{
-		*lsta = ft_lstfirst(lsta);
-		inst_rra(lsta, insts, 1);
-		return (inst_sa(lsta, insts, 1), inst_ra(lsta, insts, 2), 0);
-	}
-}
-
 ///////////////////////////////////////////////////////////////////////////////#
-t_inst	**out_scope(t_list **lsta, t_list **lstb, t_inst **insts, t_range range)//22
+t_inst	**mix_scope(t_list **lsta, t_list **lstb, t_inst **insts, t_range range)//22
 {
 	t_range	Srange;
 	t_range	Brange;
@@ -155,7 +128,17 @@ t_inst	**out_scope(t_list **lsta, t_list **lstb, t_inst **insts, t_range range)/
 	return (inst_rb(lstb, insts, Brange.l_pos));//good
 }
 
-t_inst	**sort_rest(t_list **lsta, t_list **lstb, t_inst **insts, t_range range)//27
+t_inst	**low_scope(t_list **lsta, t_list **lstb, t_inst **insts, t_range range)
+{
+	t_range	Srange;
+	t_range	Brange;
+
+	Srange = get_sec(lsta, range);
+	Brange = get_range(range, lstb);
+}
+
+////////////////////////////#
+t_inst	**high_scope(t_list **lsta, t_list **lstb, t_inst **insts, t_range range)//27 maybe merge this ft with the main
 {
 	t_range	Srange;
 	t_range	Brange;
@@ -164,35 +147,29 @@ t_inst	**sort_rest(t_list **lsta, t_list **lstb, t_inst **insts, t_range range)/
 	num = count_list(lsta);
 	Srange = get_sec(lsta, range);
 	Brange = get_range(range, lstb);
-	if (Srange.high > Brange.high)//out of scope repeats comes back here and go through the first condition 
+	//out of scope repeats comes back here and go through the first condition 
+	if (Srange.low < Brange.low)
 	{
-		if (Srange.low < Brange.low)
-		{
-			if ((num - Srange.l_pos) < Srange.l_pos)
-				inst_rra(lsta, insts, (num - Sange.l_pos));
-			else
-				inst_ra(lsta, insts, range.l_pos);
-		}
-		else//good
-		{
-			inst_rr(lsta, lstb, insts, Brange.l_pos);
-			if ((*lsta)->value != Srange.low)
-				inst_rra(lsta, insts, Srange.l_pos);
-		}
+		if ((num - Srange.l_pos) < Srange.l_pos)
+			inst_rra(lsta, insts, (num - Sange.l_pos));
+		else
+			inst_ra(lsta, insts, range.l_pos);
 	}
-	else if (Srange.low < Brange.low)
-		low_scope();//wip
-	else 
-		out_scope(lsta, lstb, insts, range);
-	return (inst_pa(lsta, lstb, insts, 1));
+	else//good
+	{
+		inst_rr(lsta, lstb, insts, Brange.l_pos);
+		if ((*lsta)->value != Srange.low)
+			inst_rra(lsta, insts, Srange.l_pos);
+	}
 }
 
-t_inst	**sort_5(t_list **lsta, t_list **lstb, t_inst **insts, int num)//24
+t_inst	**sort_5(t_list **lsta, t_list **lstb, t_inst **insts, int num)//25
 {
 	t_range range;
+	t_range	Srange;
+	t_range	Brange;
 
 	*insts = NULL;
-	range = get_range(range, lsta);
 	if (!check_sort(lsta))
 	{
 		inst_pb(lsta, lstb, insts, num - 3);
@@ -200,10 +177,19 @@ t_inst	**sort_5(t_list **lsta, t_list **lstb, t_inst **insts, int num)//24
 		sort_lsta(lsta, insts, range);
 		while (*lstb)
 		{
+			Brange = get_range(range, lstb);
+			Srange = get_sec(lsta, range);
+			if (Srange.high > Brange.high)
+				high_scope(lsta, lstb, insts, range);
+			else if (Srange.low < Brange.low)
+				low_scope(lsta, lstb, insts, range);
+			else
+				mix_scope(lsta, lstb, insts, range);
+			inst_pa(lsta, lstb, insts, 1);
 			range = get_range(range, lsta);
-			sort_rest(lsta, lstb, insts, range);
+			// sort_scope(lsta, lstb, insts, range);// 25 if you remove this
 		}
-		//this is the part  where you put it back together
+		put_back(blabloo);
 	}
 	return(insts);
 }
